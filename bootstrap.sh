4598b9e4e31fd3d1f048adb4badd0eb582bf4f04
Travis Beck <tjbenator@gmail.com>
2015-01-24 17:19:33 -0800

    Force yes on apt-get and add-apt-repository

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,4 @@
-sudo apt-get install software-properties-common
-sudo apt-add-repository ppa:ansible/ansible
+sudo apt-get install software-properties-common -y
+sudo apt-add-repository ppa:ansible/ansible -y
 sudo apt-get update
-sudo apt-get install ansible
+sudo apt-get install ansible -y
